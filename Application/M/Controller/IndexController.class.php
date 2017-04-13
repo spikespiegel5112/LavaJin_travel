942beb8727fe4ede3e010fd4d5184237cd1aa8b3
LavaJin <626600175@qq.com>
2017-04-13 08:42:11 +0800

    M

---
+++ Application/M/Controller/IndexController.class.php
@@ -0,0 +1,14 @@
+<?php
+namespace M\Controller;
+use Think\Controller;
+class IndexController extends Controller {
+    public function index(){
+
+   			 echo 'hello';
+   			 exit();
+
+
+   		 $this->display();
+       
+}
+}
