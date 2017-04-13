24f90af2b7ec4d17aefaf8bc22d0cb5dc9fb3f28
wangbolun <583288277@qq.com>
2017-04-13 16:19:43 +0800

    temp

--- FE/src/components/home.vue
+++ FE/src/components/home.vue
@@ -23,7 +23,36 @@
                 </a>
                 <label>一日游</label>
             </li>
-
+            <li>
+                <a href="javascript:;">
+                    <img src='../images/icon/TB1yRopLpXXXXaXXVXXwQxuJFXX-1025-1024.png' />
+                </a>
+                <label>一日游</label>
+            </li>
+            <li>
+                <a href="javascript:;">
+                    <img src='../images/icon/TB1yRopLpXXXXaXXVXXwQxuJFXX-1025-1024.png' />
+                </a>
+                <label>一日游</label>
+            </li>
+            <li>
+                <a href="javascript:;">
+                    <img src='../images/icon/TB1yRopLpXXXXaXXVXXwQxuJFXX-1025-1024.png' />
+                </a>
+                <label>一日游</label>
+            </li>
+            <li>
+                <a href="javascript:;">
+                    <img src='../images/icon/TB1yRopLpXXXXaXXVXXwQxuJFXX-1025-1024.png' />
+                </a>
+                <label>一日游</label>
+            </li>
+            <li>
+                <a href="javascript:;">
+                    <img src='../images/icon/TB1yRopLpXXXXaXXVXXwQxuJFXX-1025-1024.png' />
+                </a>
+                <label>一日游</label>
+            </li>
         </ul>
     </div>
     <div class='index_productlist_wrapper'>
@@ -33,6 +62,9 @@
         <ul>
             <li>
                 <div class='thumbnail'>
+                    <span class='hot'>
+                        <img class='active' src='../images/icon/hot.png' />
+                    </span>
                     <img src='../images/icon/TB1yRopLpXXXXaXXVXXwQxuJFXX-1025-1024.png' />
                     <div class='price'>
                         <span>￥298</span>
@@ -40,9 +72,130 @@
                     </div>
                 </div>
                 <div class='redpacket'>
-                    <span>使用红包再减10元</span>
+                    <span class='redpacket'></span>
+                    <label>使用红包再减10元</label>
                 </div>
                 <h5 class='title'>纳木错一日游</h5>
+            </li>
+            <li>
+                <div class='thumbnail'>
+                    <span class='hot'>
+                        <img class='active' src='../images/icon/hot.png' />
+                    </span>
+                    <img src='../images/icon/TB1yRopLpXXXXaXXVXXwQxuJFXX-1025-1024.png' />
+                    <div class='price'>
+                        <span>￥298</span>
+                        <label>￥368</label>
+                    </div>
+                </div>
+                <div class='redpacket'>
+                    <span class='redpacket'></span>
+                    <label>使用红包再减10元</label>
+                </div>
+                <h5 class='title'>纳木错一日游</h5>
+            </li>
+            <li>
+                <div class='thumbnail'>
+                    <span class='hot'>
+                        <img class='active' src='../images/icon/hot.png' />
+                    </span>
+                    <img src='../images/icon/TB1yRopLpXXXXaXXVXXwQxuJFXX-1025-1024.png' />
+                    <div class='price'>
+                        <span>￥298</span>
+                        <label>￥368</label>
+                    </div>
+                </div>
+                <div class='redpacket'>
+                    <span class='redpacket'></span>
+                    <label>使用红包再减10元</label>
+                </div>
+                <h5 class='title'>纳木错一日游</h5>
+            </li>
+        </ul>
+    </div>
+    <div class='index_productlist_wrapper'>
+        <div class='index_producttitle_item'>
+            <h2>深度体验</h2>
+        </div>
+        <ul>
+            <li>
+                <div class='thumbnail'>
+                    <span class='hot'>
+                        <img class='active' src='../images/icon/hot.png' />
+                    </span>
+                    <img src='../images/icon/TB1yRopLpXXXXaXXVXXwQxuJFXX-1025-1024.png' />
+                    <div class='price'>
+                        <span>￥298</span>
+                        <label>￥368</label>
+                    </div>
+                </div>
+                <div class='redpacket'>
+                    <span class='redpacket'></span>
+                    <label>使用红包再减10元</label>
+                </div>
+                <h5 class='title'>纳木错一日游</h5>
+            </li>
+            <li>
+                <div class='thumbnail'>
+                    <span class='hot'>
+                        <img class='active' src='../images/icon/hot.png' />
+                    </span>
+                    <img src='../images/icon/TB1yRopLpXXXXaXXVXXwQxuJFXX-1025-1024.png' />
+                    <div class='price'>
+                        <span>￥298</span>
+                        <label>￥368</label>
+                    </div>
+                </div>
+                <div class='redpacket'>
+                    <span class='redpacket'></span>
+                    <label>使用红包再减10元</label>
+                </div>
+                <h5 class='title'>纳木错一日游</h5>
+            </li>
+            <li>
+                <div class='thumbnail'>
+                    <span class='hot'>
+                        <img class='active' src='../images/icon/hot.png' />
+                    </span>
+                    <img src='../images/icon/TB1yRopLpXXXXaXXVXXwQxuJFXX-1025-1024.png' />
+                    <div class='price'>
+                        <span>￥298</span>
+                        <label>￥368</label>
+                    </div>
+                </div>
+                <div class='redpacket'>
+                    <span class='redpacket'></span>
+                    <label>使用红包再减10元</label>
+                </div>
+                <h5 class='title'>纳木错一日游</h5>
+            </li>
+        </ul>
+    </div>
+    <div class='common_nav_wrapper'>
+        <ul>
+            <li>
+                <a href='javascript:;'>
+                    <span></span>
+                    <label>首页</label>
+                </a>
+            </li>
+            <li>
+                <a href='javascript:;'>
+                    <span></span>
+                    <label>订单</label>
+                </a>
+            </li>
+            <li>
+                <a href='javascript:;'>
+                    <span></span>
+                    <label>我的</label>
+                </a>
+            </li>
+            <li>
+                <a href='javascript:;'>
+                    <span></span>
+                    <label>首页</label>
+                </a>
             </li>
         </ul>
     </div>
