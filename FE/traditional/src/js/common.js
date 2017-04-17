618ceef19576c37e6154d25a294109e6dd90fb8f
wangbolun <583288277@qq.com>
2017-04-17 15:21:37 +0800

    add static pic

--- FE/traditional/src/js/common.js
+++ FE/traditional/src/js/common.js
@@ -6,8 +6,21 @@
 					fontsize:20
 				})
 			},
+			align: function(){
+				$('.index_categorylist_wrapper li a img').align({
+					container:'.index_categorylist_wrapper li a'
+				})
+				$('.index_productlist_wrapper li .thumbnail>img').align({
+					container:'.index_productlist_wrapper li .thumbnail'
+				})
+			},
 			main: function() {
+				var that=this;
 				this.rem();
+				setTimeout(function(){
+					that.align();
+				},500)
+				
 			},
 			init: function() {
 				this.main();
