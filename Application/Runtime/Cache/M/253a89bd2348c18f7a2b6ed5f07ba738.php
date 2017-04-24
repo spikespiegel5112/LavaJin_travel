78e51b7c5b0d0e29bfd3d40cda368d8b1c2fc595
LavaJin <626600175@qq.com>
2017-04-24 10:20:06 +0800

    temp

--- Application/Runtime/Cache/M/253a89bd2348c18f7a2b6ed5f07ba738.php
+++ Application/Runtime/Cache/M/253a89bd2348c18f7a2b6ed5f07ba738.php
@@ -1,8 +1,9 @@
 <?php if (!defined('THINK_PATH')) exit();?><html>
+
 <head>
 	<meta charset="utf-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0 user-scalable=no" />
-	<title>西藏旅游</title>
+	<title>dasdasdsa</title>
 	<link rel="stylesheet" href="http://at.alicdn.com/t/font_icgfcixm8qn9izfr.css" />
 	<link rel="stylesheet" href="/travel/Public/M/assets/css/swiper.min.css" />
 	<link rel="stylesheet" href="/travel/Public/M/css/reset.css" />
@@ -38,42 +39,42 @@
 					<a href="javascript:;">
 						<img src='/travel/Public/M/images/static/02w58PICuYi_1024.jpg' />
 					</a>
-					<label>旅游线路</label>
+					<label>一日游</label>
 				</li>
 				<li>
 					<a href="javascript:;">
 						<img src='/travel/Public/M/images/static/10i58PICmeX.jpg' />
 					</a>
-					<label>租车包车</label>
+					<label>一日游</label>
 				</li>
 				<li>
 					<a href="javascript:;">
 						<img src='/travel/Public/M/images/static/73t58PICjpT_1024.jpg' />
 					</a>
-					<label>酒店预订</label>
+					<label>一日游</label>
 				</li>
 				<li>
 					<a href="javascript:;">
 						<img src='/travel/Public/M/images/static/240418-14112P9345826.jpg' />
 					</a>
-					<label>机票预订</label>
+					<label>一日游</label>
 				</li>
 				<li>
 					<a href="javascript:;">
 						<img src='/travel/Public/M/images/static/9448607_153955535000_2.jpg' />
 					</a>
-					<label>特产专区</label>
+					<label>一日游</label>
 				</li>
 				<li>
 					<a href="javascript:;">
 						<img src='/travel/Public/M/images/static/44n58PICAT2.jpg' />
 					</a>
-					<label>特色服务</label>
+					<label>一日游</label>
 				</li>
 			</ul>
 		</div>
 		<div class='index_productlist_wrapper'>
-			<div class='index_producttitle_item'>
+			<div class='common_producttitle_item'>
 				<h2>当下必玩</h2>
 			</div>
 			<ul>
@@ -82,16 +83,14 @@
 						<span class='hot'>
 						  <img class='active' src='/travel/Public/M/images/icon/hot.png' />
 					  </span>
-					  <a href="<?php echo U('Product/details');?>">
 						<img src='/travel/Public/M/images/static/73t58PICjpT_1024.jpg' />
-					</a>
 						<div class='price'>
 							<span>￥298</span>
 							<label>￥368</label>
 						</div>
 					</div>
-					<div class='redpacket'>
-						<span class='redpacket'></span>
+					<div class='common_redpacket_item'>
+						<span class='redpacketicon'></span>
 						<label>使用红包再减10元</label>
 					</div>
 					<h5 class='title'>纳木错一日游</h5>
@@ -99,7 +98,7 @@
 				<li>
 					<div class='thumbnail'>
 						<span class='hot'>
-						  <img class='active' src='/travel/Public/M/images/icon/hot.png' />
+						  <img class='active' src='images/icon/hot.png' />
 					  </span>
 						<img src='/travel/Public/M/images/static/240418-14112P9345826.jpg' />
 						<div class='price'>
@@ -107,8 +106,8 @@
 							<label>￥368</label>
 						</div>
 					</div>
-					<div class='redpacket'>
-						<span class='redpacket'></span>
+					<div class='common_redpacket_item'>
+						<span class='redpacketicon'></span>
 						<label>使用红包再减10元</label>
 					</div>
 					<h5 class='title'>纳木错一日游</h5>
@@ -116,7 +115,7 @@
 				<li>
 					<div class='thumbnail'>
 						<span class='hot'>
-						  <img class='active' src='/travel/Public/M/images/icon/hot.png' />
+						  <img class='active' src='images/icon/hot.png' />
 					  </span>
 						<img src='/travel/Public/M/images/static/73t58PICjpT_1024.jpg' />
 						<div class='price'>
@@ -124,8 +123,8 @@
 							<label>￥368</label>
 						</div>
 					</div>
-					<div class='redpacket'>
-						<span class='redpacket'></span>
+					<div class='common_redpacket_item'>
+						<span class='redpacketicon'></span>
 						<label>使用红包再减10元</label>
 					</div>
 					<h5 class='title'>纳木错一日游</h5>
@@ -133,7 +132,7 @@
 			</ul>
 		</div>
 		<div class='index_productlist_wrapper'>
-			<div class='index_producttitle_item'>
+			<div class='common_producttitle_item'>
 				<h2>深度体验</h2>
 			</div>
 			<ul>
@@ -148,8 +147,8 @@
 							<label>￥368</label>
 						</div>
 					</div>
-					<div class='redpacket'>
-						<span class='redpacket'></span>
+					<div class='common_redpacket_item'>
+						<span class='redpacketicon'></span>
 						<label>使用红包再减10元</label>
 					</div>
 					<h5 class='title'>纳木错一日游</h5>
@@ -165,8 +164,8 @@
 							<label>￥368</label>
 						</div>
 					</div>
-					<div class='redpacket'>
-						<span class='redpacket'></span>
+					<div class='common_redpacket_item'>
+						<span class='redpacketicon'></span>
 						<label>使用红包再减10元</label>
 					</div>
 					<h5 class='title'>纳木错一日游</h5>
@@ -182,8 +181,8 @@
 							<label>￥368</label>
 						</div>
 					</div>
-					<div class='redpacket'>
-						<span class='redpacket'></span>
+					<div class='common_redpacket_item'>
+						<span class='redpacketicon'></span>
 						<label>使用红包再减10元</label>
 					</div>
 					<h5 class='title'>纳木错一日游</h5>
@@ -199,8 +198,8 @@
 							<label>￥368</label>
 						</div>
 					</div>
-					<div class='redpacket'>
-						<span class='redpacket'></span>
+					<div class='common_redpacket_item'>
+						<span class='redpacketicon'></span>
 						<label>使用红包再减10元</label>
 					</div>
 					<h5 class='title'>纳木错一日游</h5>
@@ -216,8 +215,8 @@
 							<label>￥368</label>
 						</div>
 					</div>
-					<div class='redpacket'>
-						<span class='redpacket'></span>
+					<div class='common_redpacket_item'>
+						<span class='redpacketicon'></span>
 						<label>使用红包再减10元</label>
 					</div>
 					<h5 class='title'>纳木错一日游</h5>
@@ -228,21 +227,27 @@
 		<div class='common_nav_wrapper'>
 			<ul>
 				<li>
-					<a href='<?php echo U('Index/index');?>'>
+					<a href='index.html'>
 						<span class='iconfont icon-house'></span>
 						<label>首页</label>
 					</a>
 				</li>
 				<li>
-					<a href='javascript:;'>
-						<span class='iconfont icon-user2'></span>
-						<label>旅游攻略</label>
+					<a href='orderlist.html'>
+						<span class='iconfont icon-order'></span>
+						<label>订单</label>
 					</a>
 				</li>
 				<li>
-					<a href='<?php echo U('User/index');?>'>
+					<a href='user.html'>
+						<span class='iconfont icon-user2'></span>
+						<label>我的</label>
+					</a>
+				</li>
+				<li>
+					<a href='index.html'>
 						<span class='iconfont icon-house'></span>
-						<label>会员中心</label>
+						<label>首页</label>
 					</a>
 				</li>
 			</ul>
@@ -259,13 +264,19 @@
 					pagination: '.swiper-pagination'
 				})
 			},
-			align: function(){
-				$('.index_categorylist_wrapper li a img').align({
-					container:'.index_categorylist_wrapper li a'
-				})
-				$('.index_productlist_wrapper li .thumbnail>img').align({
-					container:'.index_productlist_wrapper li .thumbnail'
-				})
+			align: function() {
+				setTimeout(function() {
+					$('.index_categorylist_wrapper li a img').align({
+						container: '.index_categorylist_wrapper li a'
+					})
+				}, 500)
+
+				setTimeout(function() {
+					$('.index_productlist_wrapper li .thumbnail>img').align({
+						container: '.index_productlist_wrapper li .thumbnail'
+					})
+				}, 500)
+
 			},
 			main: function() {
 				this.align();
@@ -278,6 +289,6 @@
 		index.init();
 	})
 })(jQuery)
-
 </script>
+
 </html>
